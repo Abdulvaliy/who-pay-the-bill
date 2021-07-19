2731504fccda4e37da457f0e75405e63e5b8b2e8
Abduvali Zokhidov <80103407+Abdulvaliy@users.noreply.github.com>
2021-07-19 15:19:25 +0500

    Add files via upload

---
+++ random choyxona pay.py
@@ -0,0 +1,22 @@
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# names = ("Abdulvaliy", "Abdulboriy", "Bekhzod", "Umid", "Muhammadyusuf", "Jack", "John", "Abdusattor")
+import random
+a = random.randint(0, len(names)-1)
+print(f"Today {names[a]} pay the cost of our choyxona. Gud lak viz yor badjet ;)")
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# names = ("Abdulvaliy", "Abdulboriy", "Bekhzod", "Umid", "Muhammadyusuf", "Jack", "John", "Abdusattor")
+import random
+a = random.randint(0, len(names)-1)
+print(f"Today {names[a]} pay the cost of our choyxona. Gud lak viz yor badjet ;)")
